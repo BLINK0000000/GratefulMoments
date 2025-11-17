3669beee86b38eed13ecb3309908fe16dfb7d860
Marlon <marlon.kesr@gmail.com>
2025-11-17 13:50:08 +1300

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Marlon Keser on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
